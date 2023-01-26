3e59985a6cd7216fe37819430fc57564b9fe2afa
snow-R3 <atulkumarkumar166@gmail.com>
2023-01-26 16:13:05 +0530

    Integrate and initiat postgres database

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,7 +6,19 @@ from sqlmodel import SQLModel
 # username: postgres
 # password: admin123$
 
-DB_CONFIG = f"postgresql+asyncpg://postgres:postgres@localhost:5434/test"
+# DATABASE_URL = "postgresql://user:password@postgresserver/db"
+# DB_CONFIG = f"postgresql+asyncpg://postgres:admin123$@localhost:5432/pgDatabase"
+
+# http://127.0.0.1:8000/
+pg_user: str = "postgres" # os.getenv("SQL_USER", "")
+pg_pass: str = "admin123$" # os.getenv("POSTGRES_PASSWORD", "")
+pg_host: str =  "localhost" # os.getenv("SQL_HOST", "")
+pg_port: str =  "5432" # os.getenv("SQL_PORT", "")
+pg_database: str = "pgDatabase" # os.getenv("SQL_DB", "")
+DB_CONFIG: str = (
+    f"postgresql+asyncpg://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_database}"
+)
+
 
 
 class AsyncDatabaseSession:
@@ -23,8 +35,8 @@ class AsyncDatabaseSession:
         self.session = sessionmaker(self.engine, expire_on_commit=False, class_=AsyncSession)()
 
     async def create_all(self):
-        async with self.engine.engine() as conn:
-            await conn.run_sync(SQLModel.metadata.create_all())
+        async with self.engine.begin() as conn:
+            await conn.run_sync(SQLModel.metadata.create_all)
 
 db = AsyncDatabaseSession()
 
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,17 +1,39 @@
-from typing import Union
-
 from fastapi import FastAPI, APIRouter
-
-app = FastAPI()
-router = APIRouter()
-
-@app.get("/")
-async def read_root():
-    return {"myKye":"Wellcome to my juorny."}
+from app.config import db
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# app = FastAPI()
+# router = APIRouter()
 
-app.include_router(router)
+# @app.get("/")
+# async def read_root():
+#     return {"myKye":"Wellcome to my juorny."}
+
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
+
+# app.include_router(router)
+
+
+def init_app():
+    db.init()
+
+    app = FastAPI(
+        title= "PythonFastApiReactTailwindCSS_UserAuthentication",
+        description= "Login Page",
+        version= "1"
+    )
+
+    @app.on_event("startup")
+    async def startup():
+        await db.create_all()
+
+    @app.on_event("shutdown")
+    async def shutdown():
+        await db.close()
+
+    return app
+
+app = init_app()
