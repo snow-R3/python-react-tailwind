a29275cf630d8da9409089c9d33e512bb3e6cfb2
snow-R3 <atulkumarkumar166@gmail.com>
2023-01-28 05:08:46 +0530

    user controller created

---
+++ backend/app/controller/users.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter, Depends
+from fastapi.security import HTTPAuthorizationCredentials
+
+from app.service.schema import ResponseSchema, RegisterSchema, LoginSchema, ForgotPasswordSchema
+from app.repository.auth_repo import JWTBearer, JWTRepo
+from app.service.users import UserService
+
+router = APIRouter(
+    prefix="/users",
+    tags=["user"],
+    dependencies=[Depends(JWTBearer())]
+)
+
+@router.post("/", response_model=ResponseSchema, response_model_exclude_none=True)
+async def get_user_profile(credential: HTTPAuthorizationCredentials):
+    token = JWTRepo().extract_token(credential.credentials)
+    result = await UserService.get_user_profile(token["username"])
+    return ResponseSchema(detail="Successfull fetched data", result=result)
