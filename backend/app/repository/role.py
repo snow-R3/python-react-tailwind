fd946fc7a36b20d1a82ae7882738965cdc1b74ee
snow-R3 <atulkumarkumar166@gmail.com>
2023-01-29 16:18:45 +0530

    fixed type error <scalare_one_or_none()> to <scalar_one_or_none()> <scalares()> to <scalars()>
    Bug fixed:
    Debugging by printing steps

--- backend/app/repository/role.py
+++ backend/app/repository/role.py
@@ -11,12 +11,20 @@ class RoleRepository(BaseRepo):
     @staticmethod
     async def find_by_role_name(role_name:str):
         query = select(Role).where(Role.role_name == role_name)
-        return (await db.execute(query)).scalare_one_or_none()
+        return (await db.execute(query)).scalar_one_or_none()
 
     @staticmethod
     async def find_by_list_role_name(role_name_list:List[str]):
+
+        print(">"*10, "cb+ s (find_by_list_role_name)","<"*10,
+
+            f"\n[__class__| {__class__}]\n",
+            f"\n[{role_name_list}]\n",
+
+        ">"*10, "cb+ e (find_by_list_role_name)","<"*10)
+        
         query = select(Role).where(Role.role_name.in_(role_name_list))
-        return (db.execute(query)).scalares().all()
+        return (await db.execute(query)).scalars().all()
 
     @staticmethod
     async def create_list(role_name_list:List[str]):
