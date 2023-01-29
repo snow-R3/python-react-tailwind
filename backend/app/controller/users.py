b9f408c7ab3691c58ce1f6d6394f8aacc579dd87
snow-R3 <atulkumarkumar166@gmail.com>
2023-01-30 00:34:05 +0530

    pass Security(JWTBearer()) in method get_user_profile(credential: HTTPAuthorizationCredentials = Security(JWTBearer()))

--- backend/app/controller/users.py
+++ backend/app/controller/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Security
 from fastapi.security import HTTPAuthorizationCredentials
 
 from app.service.schema import ResponseSchema, RegisterSchema, LoginSchema, ForgotPasswordSchema
@@ -12,7 +12,7 @@ router = APIRouter(
 )
 
 @router.post("/", response_model=ResponseSchema, response_model_exclude_none=True)
-async def get_user_profile(credential: HTTPAuthorizationCredentials):
+async def get_user_profile(credential: HTTPAuthorizationCredentials = Security(JWTBearer())):
     token = JWTRepo().extract_token(credential.credentials)
     result = await UserService.get_user_profile(token["username"])
     return ResponseSchema(detail="Successfull fetched data", result=result)
