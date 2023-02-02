234aea8a5958e690afd92f465336fd6bb16554a5
snow-R3 <atulkumarkumar166@gmail.com>
2023-02-02 22:24:58 +0530

    call useState hook to set the page props

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,31 @@
+import React, { useState } from 'react';
 import './App.css';
 import Forgot from './form/Forgot';
 import Login from './form/Login';
 import Register from './form/Register';
 
 function App() {
+  const[page, setPage] = useState("login")
+  
+  const choosePage = () => { 
+    if (page === "login") {
+      return <Login setPage={setPage} />;
+    }
+    if (page === "forgot") {
+      return <Forgot setPage={setPage} />;
+    }
+    if (page === "register") {
+      return <Register setPage={setPage} />;
+    }
+  }
+
   return (
     <div className="min-h-screen bg-yellow-400 flex justify-center items-center">
       <div className="py-12 px-12 bg-white rounded-2xl shadow-xl z-20">
         {/* <Login/> */}
         {/* <Register/> */}
-        <Forgot/>
+        {/* <Forgot/> */}
+        {choosePage()}
       </div>
     </div>
   );
