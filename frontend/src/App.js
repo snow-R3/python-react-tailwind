dbce2183d94afec7859f610ec3dc09ca0907ef35
snow-R3 <atulkumarkumar166@gmail.com>
2023-03-01 00:06:26 +0530

    check token in localStorage using useEffect and render the pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Forgot from './form/Forgot';
+import Home from './form/Home';
 import Login from './form/Login';
 import Register from './form/Register';
 
 function App() {
   const[page, setPage] = useState("login")
+  const[token, setToken] = useState()
+  
+  // set auth token from localStorage if available
+  useEffect(()=> {
+    const auth = localStorage.getItem("auth_token")
+    setToken(auth)
+  }, [token])
+  
   
   const choosePage = () => { 
     if (page === "login") {
@@ -19,16 +28,24 @@ function App() {
     }
   }
 
-  return (
-    <div className="min-h-screen bg-yellow-400 flex justify-center items-center">
-      <div className="py-12 px-12 bg-white rounded-2xl shadow-xl z-20">
-        {/* <Login/> */}
-        {/* <Register/> */}
-        {/* <Forgot/> */}
-        {choosePage()}
-      </div>
-    </div>
-  );
+  // render page wrt token home/login page
+  const paegs = () => {
+    if (token == null) {
+      return (
+        <div className="min-h-screen bg-yellow-400 flex justify-center items-center">
+          <div className="py-12 px-12 bg-white rounded-2xl shadow-xl z-20">
+            {/* <Login/> */}
+            {/* <Register/> */}
+            {/* <Forgot/> */}
+            {choosePage()}
+          </div>
+        </div>
+      );
+    } else {
+      return <Home />;
+    }
+  }  
+  return <React.Fragment> { paegs() } </React.Fragment>;
 }
 
 export default App;
