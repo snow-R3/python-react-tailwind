b984778bea864c410dadddd9049cf831ff5da76c
snow-R3 <atulkumarkumar166@gmail.com>
2023-02-28 23:39:32 +0530

    import  react-toastify packaeg and css of it in inde file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ToastContainer />
       <App />
     </BrowserRouter>
   </React.StrictMode>
