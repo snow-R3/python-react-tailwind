dc2a5b96db304adcfd4261c8bedd854fc7795fc2
snow-R3 <atulkumarkumar166@gmail.com>
2023-03-07 04:00:21 +0530

    integration of forogt password to backend - create onchange funciton

--- frontend/src/form/Forgot.js
+++ frontend/src/form/Forgot.js
@@ -1,7 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
 
 export default function Forgot(props) {
+
+  const [forgotForm, setForgotForm] = useState({
+    email: "",
+    new_password: "",
+  })
+
+  const onChangeForm = (label, event) =>{
+    switch (label){
+      case "email":
+        setForgotForm({...forgotForm, email: event.target.value});
+        break;
+      case "new_password":
+        setForgotForm({...forgotForm, new_password: event.target.value});
+        break;
+    }
+  }
+  
+
   return (
     <React.Fragment>
         <h1 className="text-3xl font-bold text-center mb-4 cursor-pointer">
@@ -15,10 +37,16 @@ export default function Forgot(props) {
             <input 
                 type="email" name="" id="" placeholder="Email"
                 className="block text-sm py-3 px-4 rounded-lg w-full border outline-none focus:ring focus:outline-none focus:ring-yellow-400"
+                onChange={(event) => {
+                  onChangeForm("email", event)
+                }}
             />
             <input 
                 type="password" name="" id="" placeholder="New Password"
                 className="block text-sm py-3 px-4 rounded-lg w-full border outline-none focus:ring focus:outline-none focus:ring-yellow-400"
+                onChange={(event) => {
+                  onChangeForm("new_password", event)
+                }}
             />
           </div>
           <div className="text-center mt-6">
